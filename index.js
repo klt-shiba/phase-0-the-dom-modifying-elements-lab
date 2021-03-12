8d6f5fdeae4d67774914ab5975bb9c69c1f7c54f
Colin Tseng <klt.aus@yahoo.com>
2021-03-12 20:47:17 +1100

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+const main = document.getElementById('main');
+
+document.body.removeChild(main);
+
+const newHeader = document.getElementById('victory');
+
+newHeader.innerHTML = "<h1>YOUR-NAME is the champion!</h1>";
